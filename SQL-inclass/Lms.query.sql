2b974e9bbdda3bf4c01722820c07e476eab866fb
glcn <93012973+glcn35@users.noreply.github.com>
2022-03-18 13:26:23 +0300

    Add files via upload

---
+++ SQL-inclass/Lms.query.sql
@@ -0,0 +1,57 @@
+--1
+--List the product names in ascending order where the part from the beginning to the first space character is
+--"Samsung" in the product_name column.
+
+
+
+
+
+--2
+---Write a query that returns streets in ascending order.
+--The streets have an integer character lower than 5 after the "#" character end of the street.
+--(use sale.customer table)
+
+
+
+
+
+--3
+---Write a query that returns orders of the products branded "Seagate". 
+--It should be listed Product names and order IDs of all the products ordered or not ordered. 
+--(order_id in ascending order)
+--"Seagate" markalý ürünlerin sipariþlerini döndüren bir sorgu yazýn.  
+--Sipariþ edilen veya verilmeyen tüm ürünlerin Ürün adlarý  ve  sipariþ numaralarý listelenmelidir  . ( artan sýrada order_id )
+
+
+
+
+
+
+
+
+--4
+--Write a query that returns the order date of the product named "Sony - 5.1-Ch. 3D / Smart Blu-ray Home Theater System - Black"--
+--"Sony - 5.1-Ch. 3D / Smart Blu-ray Ev Sinema Sistemi - Siyah" isimli ürünün sipariþ tarihini döndüren bir sorgu yazýnýz.
+
+
+
+
+
+
+--5
+--Please write a query to return only the order ids that have an average amount of more than $2000. 
+--Your result set should include order_id. Sort the order_id in ascending order.
+
+--Lütfen yalnýzca ortalama tutarý 2000 ABD Dolarýndan fazla olan sipariþ kimliklerini döndürmek için bir sorgu yazýn. 
+---Sonuç kümeniz order_id içermelidir. order_id'yi artan düzende sýralayýn.
+
+
+
+
+
+--6
+--Write a query that returns the count of orders of each day between '2020-01-19' and '2020-01-25'. Report the result using Pivot Table.
+--Note: The column names should be day names (Sun, Mon, etc.).
+--(Use SampleRetail DB on SQL Server and paste the result in the answer box.
+--'2020-01-19' ve '2020-01-25' arasýndaki her günün sipariþ sayýsýný döndüren bir sorgu yazýn. Özet Tabloyu kullanarak sonucu bildirin.
+--Not : Sütun adlarý gün adlarý olmalýdýr (Paz, Pzt, vb.).
